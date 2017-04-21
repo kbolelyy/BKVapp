5649669231fa25fc964ecfb122862396f73dbc0b
kbolelyy <kbolelyy@gmail.com>
2017-04-22 01:33:18 +0700

    configuration for DB

---
+++ hibernate/src/main/java/testHibernate.java
@@ -0,0 +1,14 @@
+import org.hibernate.SessionFactory;
+
+/**
+ * Created by KBolelyy on 21.04.2017.
+ */
+public class testHibernate {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    }
+
+}
